f6d76f750e1c02e4b3120e0601d1456cb616d11d
Matetorture <student@student.machine>
2024-09-25 05:40:35 +0000

    commit from StudentMachine

--- cpp/benchmark_sortowanie/main_test.cpp
+++ cpp/benchmark_sortowanie/main_test.cpp
@@ -33,7 +33,8 @@ TEST(BenchmarkTest, QuickSortBenchmark) {
     // Początek pomiaru czasu
     auto start = std::chrono::high_resolution_clock::now();
 
-    quickSort(vec); // Sortujemy wektor
+	//bubbleSort(vec);
+	quickSort(vec, 0, vec.size() - 1);
 
     // Koniec pomiaru czasu
     auto end = std::chrono::high_resolution_clock::now();
@@ -41,6 +42,26 @@ TEST(BenchmarkTest, QuickSortBenchmark) {
 
     std::cout << "Czas sortowania: " << elapsed_seconds.count() << "s\n";
 }
+
+TEST(BenchmarkTest, BubbleSortBenchmark) {
+    std::vector<int> vec(10000); // Tworzymy wektor z 10000 losowymi liczbami
+    std::srand(std::time(0)); // Inicjalizacja generatora liczb losowych
+    std::generate(vec.begin(), vec.end(), std::rand);
+
+    // Początek pomiaru czasu
+    auto start = std::chrono::high_resolution_clock::now();
+
+	bubbleSort(vec);
+	//quickSort(vec, 0, vec.size() - 1);
+
+    // Koniec pomiaru czasu
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+
+    std::cout << "Czas sortowania: " << elapsed_seconds.count() << "s\n";
+}
+
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
