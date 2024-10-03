acf3733919f63d9e6bf35d7882e46baaee9dd237
Przemysław Pytlarz <93830296+Matetorture@users.noreply.github.com>
2024-10-03 07:24:18 +0100

    Update functions.cpp

--- cpp/unit_tests_fizzbuzz/functions.cpp
+++ cpp/unit_tests_fizzbuzz/functions.cpp
@@ -1,2 +1,11 @@
-
-#TODO: tu umieść swoją funkcję
+string FizzBuzz(int n){
+  if(n>=1){
+    if(n%15==0){
+      return "FizzBuzz";
+    }else if(n%3==0){
+      return "Fizz";
+    }else if(n%5==0){
+      return "Buzz";
+    }
+  }
+}
