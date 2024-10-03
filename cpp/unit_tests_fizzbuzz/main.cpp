8406ecc4345dc1d53b5e4ea8d43fcc56f93f5dfc
Przemysław Pytlarz <93830296+Matetorture@users.noreply.github.com>
2024-10-03 07:26:05 +0100

    Update main.cpp

--- cpp/unit_tests_fizzbuzz/main.cpp
+++ cpp/unit_tests_fizzbuzz/main.cpp
@@ -4,7 +4,23 @@
 using namespace std;
 
 int main() {
-    #TODO: tu napisz program
+    cout<<"-=-=-=-=-=-=-=-=-"<<endl;
+    cout<<FizzBuzz(1)<<endl;
+    cout<<FizzBuzz(2)<<endl;
+    cout<<FizzBuzz(3)<<endl;
+    cout<<FizzBuzz(4)<<endl;
+    cout<<FizzBuzz(5)<<endl;
+    cout<<FizzBuzz(6)<<endl;
+    cout<<FizzBuzz(7)<<endl;
+    cout<<FizzBuzz(8)<<endl;
+    cout<<FizzBuzz(9)<<endl;
+    cout<<FizzBuzz(10)<<endl;
+    cout<<FizzBuzz(11)<<endl;
+    cout<<FizzBuzz(12)<<endl;
+    cout<<FizzBuzz(13)<<endl;
+    cout<<FizzBuzz(14)<<endl;
+    cout<<FizzBuzz(15)<<endl;
+    cout<<"-=-=-=-=-=-=-=-=-"<<endl;
     return 0;
 }
 
