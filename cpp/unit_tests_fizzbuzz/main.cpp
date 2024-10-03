1ef6c08b065ca54c560efc397258d28997429758
Przemysław Pytlarz <93830296+Matetorture@users.noreply.github.com>
2024-10-03 07:32:39 +0100

    Update main.cpp

--- cpp/unit_tests_fizzbuzz/main.cpp
+++ cpp/unit_tests_fizzbuzz/main.cpp
@@ -5,21 +5,21 @@ using namespace std;
 
 int main() {
     cout<<"-=-=-=-=-=-=-=-=-"<<endl;
-    cout<<FizzBuzz(1)<<endl;
-    cout<<FizzBuzz(2)<<endl;
-    cout<<FizzBuzz(3)<<endl;
-    cout<<FizzBuzz(4)<<endl;
-    cout<<FizzBuzz(5)<<endl;
-    cout<<FizzBuzz(6)<<endl;
-    cout<<FizzBuzz(7)<<endl;
-    cout<<FizzBuzz(8)<<endl;
-    cout<<FizzBuzz(9)<<endl;
-    cout<<FizzBuzz(10)<<endl;
-    cout<<FizzBuzz(11)<<endl;
-    cout<<FizzBuzz(12)<<endl;
-    cout<<FizzBuzz(13)<<endl;
-    cout<<FizzBuzz(14)<<endl;
-    cout<<FizzBuzz(15)<<endl;
+    cout<<1<<" "<<FizzBuzz(1)<<endl;
+    cout<<2<<" "<<FizzBuzz(2)<<endl;
+    cout<<3<<" "<<FizzBuzz(3)<<endl;
+    cout<<4<<" "<<FizzBuzz(4)<<endl;
+    cout<<5<<" "<<FizzBuzz(5)<<endl;
+    cout<<6<<" "<<FizzBuzz(6)<<endl;
+    cout<<7<<" "<<FizzBuzz(7)<<endl;
+    cout<<8<<" "<<FizzBuzz(8)<<endl;
+    cout<<9<<" "<<FizzBuzz(9)<<endl;
+    cout<<10<<" "<<FizzBuzz(10)<<endl;
+    cout<<11<<" "<<FizzBuzz(11)<<endl;
+    cout<<12<<" "<<FizzBuzz(12)<<endl;
+    cout<<13<<" "<<FizzBuzz(13)<<endl;
+    cout<<14<<" "<<FizzBuzz(14)<<endl;
+    cout<<15<<" "<<FizzBuzz(15)<<endl;
     cout<<"-=-=-=-=-=-=-=-=-"<<endl;
     return 0;
 }
