14d8e3aac42bb6fa3945d2a5d34b6788abd55284
Matetorture <student@student.machine>
2024-10-24 06:38:22 +0000

    studentmachine send

--- cpp/schody/main.cpp
+++ cpp/schody/main.cpp
@@ -6,22 +6,18 @@ using namespace std;
 int main() {
     double Sh, Sl;
     
-    // Pobieranie wartości od użytkownika
     cout << "Podaj całkowitą wysokość schodów (Sh): ";
     cin >> Sh;
     cout << "Podaj całkowitą głębokość schodów (Sl): ";
     cin >> Sl;
 
-    // Wywołanie funkcji obliczającej schody
     StairsResult wynik = calculate_stairs(Sh, Sl);
 
-    // Sprawdzamy, czy operacja się powiodła
     if (wynik.success) {
         cout << "Ilość stopni: " << wynik.num_steps << endl;
         cout << "Wysokość stopnia: " << wynik.step_height << " cm" << endl;
         cout << "Głębokość stopnia: " << wynik.step_depth << " cm" << endl;
     } else {
-        // Wyświetlamy wiadomość o błędzie
         cout << wynik.message << endl;
     }
 
--- cpp/schody/main_test.cpp
+++ cpp/schody/main_test.cpp
@@ -2,12 +2,56 @@
 #include <gtest/gtest.h>
 #include "functions.cpp"
 
+//możliwe
 TEST(calculate_stairs, schodyMozliwe0) {
     StairsResult wynik = calculate_stairs(180, 500);
-    EXPECT_EQ(wynik, {9, 20.0, 55.55, true, "Sukces"});
+    StairsResult oczekiwany = {10, 18, 50, true, "Sukces"};
+    EXPECT_EQ(wynik, oczekiwany);
+}
+TEST(calculate_stairs, schodyMozliwe1) {
+    StairsResult wynik = calculate_stairs(152, 400);
+    StairsResult oczekiwany = {8, 19, 50, true, "Sukces"};
+    EXPECT_EQ(wynik, oczekiwany);
+}
+TEST(calculate_stairs, schodyMozliwe2) {
+    StairsResult wynik = calculate_stairs(95, 300);
+    StairsResult oczekiwany = {5, 19, 60, true, "Sukces"};
+    EXPECT_EQ(wynik, oczekiwany);
 }
 
+//zła głębokość
+TEST(calculate_stairs, schodyGlebokosc0) {
+    StairsResult wynik = calculate_stairs(190, 400);
+    StairsResult oczekiwany = {0, 0, 0, false, "Całkowita głębokość schodów przekracza dopuszczalną."};
+    EXPECT_EQ(wynik, oczekiwany);
+}
+TEST(calculate_stairs, schodyGlebokosc1) {
+    StairsResult wynik = calculate_stairs(200, 300);
+    StairsResult oczekiwany = {0, 0, 0, false, "Całkowita głębokość schodów przekracza dopuszczalną."};
+    EXPECT_EQ(wynik, oczekiwany);
+}
+TEST(calculate_stairs, schodyGlebokosc2) {
+    StairsResult wynik = calculate_stairs(160, 320);
+    StairsResult oczekiwany = {0, 0, 0, false, "Całkowita głębokość schodów przekracza dopuszczalną."};
+    EXPECT_EQ(wynik, oczekiwany);
+}
 
+//zła wysokość
+TEST(calculate_stairs, schodyWysokosc0) {
+    StairsResult wynik = calculate_stairs(120, 500);
+    StairsResult oczekiwany = {0, 0, 0, false, "Nie można dopasować wysokości schodów do wymaganych warunków."};
+    EXPECT_EQ(wynik, oczekiwany);
+}
+TEST(calculate_stairs, schodyWysokosc1) {
+    StairsResult wynik = calculate_stairs(60, 300);
+    StairsResult oczekiwany = {0, 0, 0, false, "Nie można dopasować wysokości schodów do wymaganych warunków."};
+    EXPECT_EQ(wynik, oczekiwany);
+}
+TEST(calculate_stairs, schodyWysokosc2) {
+    StairsResult wynik = calculate_stairs(290, 10000);
+    StairsResult oczekiwany = {0, 0, 0, false, "Nie można dopasować wysokości schodów do wymaganych warunków."};
+    EXPECT_EQ(wynik, oczekiwany);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
