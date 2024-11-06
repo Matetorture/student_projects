92572fd4505f892257de79f92ca3428947943cd4
Matetorture <student@student.machine>
2024-11-06 07:45:56 +0000

    commit from StudentMachine

--- cpp/schody/main_test.cpp
+++ cpp/schody/main_test.cpp
@@ -3,7 +3,7 @@
 
 TEST(calculate_stairs, schody0) {
     StairsResult wynik = calculate_stairs(100, 140);
-    StairsResult oczekiwany = {5, 17, 28};
+    StairsResult oczekiwany = {5, 16.67, 27.99};
     EXPECT_EQ(wynik, oczekiwany);
 }
 TEST(calculate_stairs, schody1) {
@@ -13,12 +13,12 @@ TEST(calculate_stairs, schody1) {
 }
 TEST(calculate_stairs, schody2) {
     StairsResult wynik = calculate_stairs(190, 250);
-    StairsResult oczekiwany = {10, 18, 25};
+    StairsResult oczekiwany = {10, 17.28, 25.00};
     EXPECT_EQ(wynik, oczekiwany);
 }
 TEST(calculate_stairs, schody3) {
     StairsResult wynik = calculate_stairs(200, 280);
-    StairsResult oczekiwany = {10, 19, 28};
+    StairsResult oczekiwany = {11, 16.67, 25.45};
     EXPECT_EQ(wynik, oczekiwany);
 }
 TEST(calculate_stairs, schody4) {
@@ -28,7 +28,7 @@ TEST(calculate_stairs, schody4) {
 }
 TEST(calculate_stairs, schody5) {
     StairsResult wynik = calculate_stairs(380, 500);
-    StairsResult oczekiwany = {20, 19, 25};
+    StairsResult oczekiwany = {20, 18.1, 25};
     EXPECT_EQ(wynik, oczekiwany);
 }
 
